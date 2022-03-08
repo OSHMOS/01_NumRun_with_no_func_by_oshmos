e78a2d3f95413b9b86248b7659e2814593c34272
OSHMOS <ojames9707@gmail.com>
2022-03-08 11:58:33 +0900

    숫자 달리기 기본 베이스

--- 01_NumRun_with_no_func/Program.cs
+++ 01_NumRun_with_no_func/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace _NumRun_with_no_func
 {
@@ -6,7 +7,52 @@ namespace _NumRun_with_no_func
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            const string LINE = "----------------------------------";
+
+            int runA = 0;
+            int runB = 0;
+            int runC = 0;
+            int runD = 0;
+
+            while (true)
+            {
+                Thread.Sleep(1000); // 딜레이 (1000 = 1초)
+                Console.Clear(); // 화면 지우기
+
+                ++runA;
+                ++runB;
+                ++runC;
+                ++runD;
+
+                Console.WriteLine(LINE);
+
+                for (int i = 0; i < runA; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("1");
+
+                for (int i = 0; i < runB; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("2");
+
+                for (int i = 0; i < runC; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("3");
+
+                for (int i = 0; i < runD; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("4");
+
+                Console.WriteLine(LINE);
+            }
         }
     }
 }
